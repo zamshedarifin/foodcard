686a8da79688245100655696247ece12533f7366
zamshedarifin <zamshedularifin@gmail.com>
2022-02-03 10:56:08 +0600

    first commit

---
+++ resources/views/admin/layouts/navbar.blade.php
@@ -0,0 +1,23 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+    <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <i class="icon-grid menu-icon"></i>
+                <span class="menu-title">Dashboard</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+                <i class="icon-layout menu-icon"></i>
+                <span class="menu-title">UI Elements</span>
+                <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="ui-basic">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"> <a class="nav-link" href="#">Buttons</a></li>
+                    <li class="nav-item"> <a class="nav-link" href="@">Dropdowns</a></li>
+                </ul>
+            </div>
+        </li>
+    </ul>
+</nav>
