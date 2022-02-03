1337a756aa32fcdc0398bcc049f34ca577e53b87
zamshedarifin <zamshedularifin@gmail.com>
2022-02-03 10:58:03 +0600

    first commit

--- routes/web.php
+++ routes/web.php
@@ -7,20 +7,7 @@ Route::get('/', function () {
 });
 
 
-Route::namespace('User')->prefix('user')->name('user.')->group(function () {
-    Route::namespace('Auth')->group(function () {
-        Route::get('/login', 'LoginController@showLoginForm')->name('login');
-        Route::post('/login', 'LoginController@login')->name('login');
-        Route::get('/logout', 'LoginController@logout')->name('logout');
 
-        Route::get('/register', 'RegisterController@showRegistrationForm')->name('register');
-        Route::post('/register', 'RegisterController@register');
-    });
-
-    Route::middleware('auth')->group(function () {
-        Route::get('profile', 'UserController@index')->name('profile');
-    });
-});
 
 Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function () {
     Route::namespace('Auth')->group(function () {
